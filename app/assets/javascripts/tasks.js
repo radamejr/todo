7f8a2a3660fd98a1375b067f737438ed0d11db37
radamejr <radamejr@gmail.com>
2019-09-04 17:26:05 +0000

    Completed functionality of the App. Includes: Adding tasks, completing tasks, adding strikethrough, and CSS formating

---
+++ app/assets/javascripts/tasks.js
@@ -0,0 +1,70 @@
+$(function() {
+    
+    // the task Html method takes in a JavaScript representation
+    // of the task and produces an HTML representation using
+    // <li> tags
+
+    function taskHtml(task) {
+      var checkedStatus = task.done ? "checked" : "";
+      var liClass = task.done ? "completed" : "";
+      var liElement = '<li id="listItem-' + task.id + '" class="' + liClass + '">' +
+      '<div class="view"><input class="toggle" type="checkbox"' +
+       " data-id='" + task.id + "'" +
+        checkedStatus +
+        '><label>' + 
+        task.title + 
+        '</label></div></li>';
+
+      return liElement;
+    }
+
+    function toggleTask(e) {
+        var itemID = $(e.target).data("id");
+       
+
+        var doneValue = Boolean($(e.target).is(":checked"));
+       
+
+        $.post("/tasks/" + itemID, {
+          _method: "PUT",
+          task: {
+            done: doneValue
+          }
+        }).success(function(data) {
+          var liHtml = taskHtml(data);
+          var $li = $("#listItem-" + data.id);
+          $li.replaceWith(liHtml);
+          $('.toggle').change(toggleTask);
+        });
+      }
+
+    $.get("/tasks").success( function( data ) {
+      var htmlString = "";
+
+      $.each(data, function(index, task) {
+        htmlString += taskHtml(task);
+      });
+      var ulTodos = $('.todo-list');
+      ulTodos.html(htmlString);
+
+      $('.toggle').change(toggleTask);
+    
+    });
+
+    $('#new-form').submit(function(event) {
+      event.preventDefault();
+      var textbox = $('.new-todo');
+      var payload = {
+        task: {
+          title: textbox.val()
+        }
+      };
+      $.post("/tasks", payload).success(function(data) {
+        var htmlString = taskHtml(data);
+        var ulTodos = $('.todo-list');
+        ulTodos.append(htmlString);
+        $('.toggle').click(toggleTask);
+        $('.new-todo').val('');
+      });
+    });
+});
--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -5,10 +5,16 @@ RSpec.describe TasksController, type: :controller do
     it "should list the tasks in the database" do
       task1 = FactoryBot.create(:task)
       task2 = FactoryBot.create(:task)
+      task1.update_attributes(title: "Something else")
       get :index
       expect(response).to have_http_status :success
       response_value = ActiveSupport::JSON.decode(@response.body)
       expect(response_value.count).to eq(2)
+      response_ids = []
+      response_ids = response_value.collect do |task|
+        task["id"]
+      end
+      expect(response_ids).to eq([task1.id, task2.id])      
     end
   end
 
@@ -19,6 +25,16 @@ RSpec.describe TasksController, type: :controller do
       expect(response).to have_http_status(:success)
       task.reload
       expect(task.done).to eq(true)
+    end
+  end
+
+  describe "tasks#create" do
+    it "should allow new tasks to be created" do
+      post :create, params: {task: {title: "fix things"}}
+      expect(response).to have_http_status(:success)
+      response_value = ActiveSupport::JSON.decode(@response.body)
+      expect(response_value['title']).to eq("fix things")
+      expect(Task.last.title).to eq("fix things")
     end
   end
 
